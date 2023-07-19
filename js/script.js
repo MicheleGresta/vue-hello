22521c95abe49b239754d164df02a9c9bb03712e
MicheleGresta <malachia_1997@hotmail.it>
2023-07-19 15:23:35 +0200

    aggiunto messaggio, non trovo per le immagini

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+data() {
+
+    return {
+
+            messaggioH1: "NON CLICCARE IL BOTTONE",
+            messaggioBtn: "Non mi cliccare",
+    }
+    
+    
+},
+
+methods: {
+    
+}
+
+
+
+}) 
+console.log(app);
+
+app.mount("#app");
